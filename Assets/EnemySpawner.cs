3e4d375cc8d77d563c51d3660869d201698560e4
ChenOku <chenoku@gmail.com>
2025-03-17 13:50:39 -0500

    spawn fix

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -7,13 +7,15 @@ public class EnemySpawner : MonoBehaviour
 {
 
     public GameObject tortuga; // Se asigna en el Inspector
+    public Transform[] spawnPoints; // Array de puntos de spawn
+
 
     public float spawnTime;
     public float spawnTimeRandom;
 
     private float spawnTimer;
-    private NavMeshAgent nav;
-    public Transform target;
+   // private NavMeshAgent nav;
+    //public Transform target;
     private Vector3 location;
 
     // Start is called before the first frame update
@@ -23,7 +25,10 @@ public class EnemySpawner : MonoBehaviour
         {
             Debug.LogError("No se ha asignado el prefab de la tortuga en el Inspector.");
         }
-        location = new Vector3(89, 10, 9);
+        if (spawnPoints.Length == 0)
+        {
+            Debug.LogError("No se han asignado puntos de spawn en el Inspector.");
+        }
 
         ResetSpawnTimer();
     }
@@ -32,14 +37,31 @@ public class EnemySpawner : MonoBehaviour
     void Update()
     {
         spawnTimer -= Time.deltaTime;
-        target = GameObject.FindGameObjectWithTag("PlayerSak").transform;
+       // target = GameObject.FindGameObjectWithTag("PlayerSak").transform;
+        // if (spawnTimer <= 0.0f)
+        // {
+        //     Instantiate(tortuga, location, Quaternion.identity);
+
+            //ResetSpawnTimer();
         if (spawnTimer <= 0.0f)
         {
-            Instantiate(tortuga, location, Quaternion.identity);
-
-            ResetSpawnTimer();
+           SpawnEnemy();
+           ResetSpawnTimer();
         }
     }
+
+    void SpawnEnemy()
+    {
+        if (spawnPoints.Length == 0) return;
+
+        // Seleccionar un punto de spawn aleatorio
+        int spawnIndex = Random.Range(0, spawnPoints.Length);
+        Transform spawnPoint = spawnPoints[spawnIndex];
+
+        // Instanciar el enemigo en el punto de spawn seleccionado
+        Instantiate(tortuga, spawnPoint.position, spawnPoint.rotation);
+    } 
+
 
     void ResetSpawnTimer()
     {
